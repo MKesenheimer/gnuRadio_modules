b8270d1ad85cc99356ba8f97da62f3ba819aaed0
MKesenheimer <mkesenheimer@gmx.net>
2021-04-03 18:31:13 +0200

    small changes

--- gr-OOK_demodulator/apps/OOK_test.py
+++ gr-OOK_demodulator/apps/OOK_test.py
@@ -30,7 +30,6 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
-from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
 import signal
@@ -104,10 +103,8 @@ class OOK_test(gr.top_block, Qt.QWidget):
         self.blocks_vector_source_x_0 = blocks.vector_source_c(data, True, 1, [])
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, 600)
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/kesenheimer/Documents/Basteln/SDR/gnuRadio_modules/OOK_test/OOK_test.bin', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/Users/kesenheimer/Documents/Basteln/SDR/gnuRadio_modules/gr-OOK_demodulator/apps/OOK_test.bin', False)
         self.blocks_file_sink_0.set_unbuffered(False)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, freq_0, 1, 0, 0)
         self.OOKDemodulator_1 = OOKDemodulator(
             freq_0=freq_0,
             gain=2,
@@ -124,10 +121,8 @@ class OOK_test(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.OOKDemodulator_1, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.OOKDemodulator_1, 1), (self.qtgui_sink_x_1, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.OOKDemodulator_1, 0))
         self.connect((self.blocks_repeat_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.OOKDemodulator_1, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
 
 
@@ -142,7 +137,6 @@ class OOK_test(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.OOKDemodulator_1.set_samp_rate(self.samp_rate)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_sink_x_1.set_frequency_range(0, self.samp_rate)
 
@@ -158,7 +152,6 @@ class OOK_test(gr.top_block, Qt.QWidget):
     def set_freq_0(self, freq_0):
         self.freq_0 = freq_0
         self.OOKDemodulator_1.set_freq_0(self.freq_0)
-        self.analog_sig_source_x_0.set_frequency(self.freq_0)
 
     def get_data(self):
         return self.data
